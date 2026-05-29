3d785d128119a5866b4d44f8a05de9ccb054db5c
Larry F. Noe <amanshukla131@gmail.com>
2026-05-29 21:46:40 +0530

    Fix typo in CLIENT_DOWNLOAD_URL filename

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -7,7 +7,7 @@
 
 export const SERVER_DOWNLOAD_URL = "https://github.com/shreyash1x/tailhub/releases/download/Server-V1.0.0/TailHub-Server.exe";
 
-export const CLIENT_DOWNLOAD_URL = "https://github.com/shreyash1x/tailhub/releases/download/v1.0.0/TailHub-Cliet.exe";
+export const CLIENT_DOWNLOAD_URL = "https://github.com/shreyash1x/tailhub/releases/download/v1.0.0/TailHub-Client.exe";
 
 // Helpful links
 export const DOCUMENTATION_URL = "https://github.com/Kanha/USB/tree/main/docs";
